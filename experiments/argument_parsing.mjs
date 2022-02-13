526f5c3a169bf809c40d792c87075b01e3cf17aa
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2022-02-13 23:52:53 +0100

    Made working example for argumetns_to_bins_and_settings

--- experiments/argument_parsing.mjs
+++ experiments/argument_parsing.mjs
@@ -4,33 +4,30 @@ export function arguments_to_bins_and_settings() {
 	const argument_array = new Array(... arguments);
 	const base_list = argument_array.slice(1);
 
-	const settings = new Map();
+	const settings = new Object();
 	const result_bins = new Map();
-	result_bins[constants.SETTINGS] = settings;
-	 new Map();
 	for (const base of base_list) {
-		result_bins[base] = new Array();
+		result_bins.set(base, new Array());
 	}
+	result_bins.set(constants.SETTINGS, settings);
 
 	for (const arg of argument_array[0]) {
 
 		let found = false;
 		for (const base of base_list) {
 			if (arg instanceof base) {
-				result_bins[base].push(arg);
+				result_bins.get(base).push(arg);
 				found = true;
 				break;
 			}
 		}
 
 		if (!found) {
-			for (const [key, value] in Object.entries(arg)) {
+			for (const [key, value] of Object.entries(arg)) {
 				settings[key] = value;
 			}
-
 		}
-
 	}
 
-	return result_bins;
+	return result_bins.values();
 }
--- experiments/ui_implementation.mjs
+++ experiments/ui_implementation.mjs
@@ -3,13 +3,10 @@ import { arguments_to_bins_and_settings } from './argument_parsing.mjs';
 
 export function implement_ui() {
 
-	return arguments_to_bins_and_settings(arguments, UI.base);
+	const [ui_elements, settings] = arguments_to_bins_and_settings(arguments, UI.base);
 
-	const ui_elements = new Array();
-	const settings = new Map();
-
-
-	console.log(ui_elements, settings);
+	console.log(ui_elements);
+	console.log(settings)
 
 	return
 }
